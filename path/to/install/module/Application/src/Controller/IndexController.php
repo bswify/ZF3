fae9980ea8064fea09d05ac7095b9fd71a7758d3
bswify <34600468+bswify@users.noreply.github.com>
2019-07-25 14:53:58 +0700

    get list Restaurants

--- path/to/install/module/Application/src/Controller/IndexController.php
+++ path/to/install/module/Application/src/Controller/IndexController.php
@@ -15,7 +15,24 @@ class IndexController extends AbstractActionController
     public function indexAction()
     {
 
-       
+        // $url = "https://app.jasmine.com/jpmapi/workschedule/v1/employeeid/".$employeeid2."/workdate"."/".$workdate;
+        
+        $url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.803011,100.538813&radius=1500&type=restaurant&keyword=Bangsue&key=AIzaSyDtbkvX3zjP15y_1dQWzoxvAlMLsgJSEuw";
+        $headers = array(
+        'Content-type: application/json; charset=UTF-8'
+        );
+         $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPGET , 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        echo $response ; exit();
+        // return $response;
+        // return $this->render('index');
+        // session_start() ;
+        // $_SESSION['response'] =$response;
         return new ViewModel();
     }
 }
