73c54cbf487959e0e5bd85b9a43edbf19763928f
bswify <34600468+bswify@users.noreply.github.com>
2019-07-26 11:21:39 +0700

    1

--- path/to/install/module/Application/src/Controller/IndexController.php
+++ path/to/install/module/Application/src/Controller/IndexController.php
@@ -7,34 +7,20 @@
 
 namespace Application\Controller;
 
+
+
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+
     public function indexAction()
     {
-        //เรียก api เอาข้อมูลร้านอาหาร  ต้องรับ location มาจากหน้า index  ตอนนี้  รับมาไม่เป้น เลยเซตไปก่อน
-        $lat = 13.803011;
-        $long = 100.538813;
-        $keyword = "Bangsue";
 
-        $url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$lat.",".$long."&radius=1500&type=restaurant&keyword=".$keyword."&key=AIzaSyDtbkvX3zjP15y_1dQWzoxvAlMLsgJSEuw";
-        $headers = array(
-        'Content-type: application/json; charset=UTF-8'
-        );
-         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPGET , 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $response = curl_exec($ch);
-        $character = json_decode($response);
-        // จะส่งข้อมูล $response ไปแสเงใน table ในหน้า index
-//         echo $response ; exit();
-//        return  $response;
          return new ViewModel();
 
     }
+
+
 }
